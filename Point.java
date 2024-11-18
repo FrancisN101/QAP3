461ec1da144721579081779b109e76e01f846527
FrancisN101 <162461843+FrancisN101@users.noreply.github.com>
2024-11-18 22:20:32 +0500

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,44 @@
+public class Point {
+    // Private instance variables
+    private float x = 0.0f;
+    private float y = 0.0f;
+ 
+    // Constructors
+    public Point() {}
+ 
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+    // Getter and setter methods
+    public float getX() {
+        return x;
+    }
+ 
+    public void setX(float x) {
+        this.x = x;
+    }
+ 
+    public float getY() {
+        return y;
+    }
+ 
+    public void setY(float y) {
+        this.y = y;
+    }
+ 
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+    public float[] getXY() {
+        return new float[] { x, y };
+    }
+ 
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
